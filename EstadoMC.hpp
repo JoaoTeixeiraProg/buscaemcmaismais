80a907e58638579a78c1ec5b077e7354f1e5be96
João Teixeira <107081667+JoaoTeixeiraProg@users.noreply.github.com>
2022-09-12 20:27:11 -0300

    Create EstadoMC.hpp

---
+++ EstadoMC.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "Estado.hpp"
+
+class EstadoMC : public Estado{
+private:
+    int mD; //missionario margem direita
+    int cD; //canibais margem direita
+    int mE; //missionario margem esquerda
+    int cE; //canibais margem esquerda
+    char barco; //onde barco esta
+   
+public:
+    EstadoMC(int mE, int cE, int mD, int cD, char barco);
+    bool eObjetivo() = 0;
+list<Estado *> expandir() = 0;
+void imprimeEstado();
+};
