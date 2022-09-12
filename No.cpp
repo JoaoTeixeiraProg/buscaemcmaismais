080f44d53416a4087f3b6bab12230805d5ed7901
João Teixeira <107081667+JoaoTeixeiraProg@users.noreply.github.com>
2022-09-12 20:20:36 -0300

    Create No.cpp

---
+++ No.cpp
@@ -0,0 +1,19 @@
+#include "No.hpp"
+
+No::No(Estado * estado){
+	this->estado = estado;
+	this->pai = NULL;
+}
+	
+No::No(Estado * estado, No * pai){
+	this->estado = estado;
+	this->pai = pai;
+}
+	
+Estado * No::getEstado(){
+	return this->estado;
+}
+
+No * No::getPai(){
+	return this->pai;
+}
