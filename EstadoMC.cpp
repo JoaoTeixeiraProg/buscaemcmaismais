23c627bfbcb55818a58ad9d5e1011efd3a8e576c
João Teixeira <107081667+JoaoTeixeiraProg@users.noreply.github.com>
2022-09-12 20:48:21 -0300

    Update EstadoMC.cpp

--- EstadoMC.cpp
+++ EstadoMC.cpp
@@ -21,6 +21,21 @@ bool EstadoMC::bool ePermitido(int mE, int cE, int mD, int cD, char barco){
         return false;
     
     return (mE >= cE || (mE == 0 && cE > 0)) && (mD >= cD || (mD == 0 && cD > 0 ));
+}
+
+Estado * EstadoMC::levar2Missionarios(){
+    if(this -> barco == 'e'){
+        if(this -> ePermitido(this -> mE - 2, this -> cE, this -> mD + 2, this -> cD, 'd')){
+            return new EstadoMC(this -> mE - 2, this -> cE, this -> mD + 2, this -> cD, 'd');
+        }
+        return NULL;
+    }else{
+        if(this -> ePermitido(this -> mE + 2, this -> cE, this -> mD - 2, this -> cD, 'e')){
+            return new EstadoMC(this -> ePermitido(this -> mE + 2, this -> cE, this -> mD - 2, this -> cD, 'e');
+        }
+        
+    }
+    
     
 }
 
